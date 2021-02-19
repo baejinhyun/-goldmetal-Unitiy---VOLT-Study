b09b2488b07298cac4137cc52ec6ab47ba3a157d
baejinhyun <jinjinjara@kakao.com>
2021-02-19 17:25:59 +0900

    First commit

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Vector3[] PointList;
+
+    public RuntimeAnimatorController[] LevelAc;
+
+    public void ChangeAc(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = LevelAc[level - 1];
+    }
+
+}
